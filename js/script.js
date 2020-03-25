10bcaed300652e5df997ee62e5d119b5ad835215
kwamekert <tomriddle477@gmail.com>
2020-03-25 12:56:43 +0000

    Styles updated for site

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-script.js
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
